b57b02fd76dee6ca0d8051f40cba3acbba190c8f
RewanNaguib <rewan.mnaguib@gmail.com>
2022-10-22 04:24:35 +0200

    running a server using Express and creating a simple endpoint to test the server is working

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,14 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var arrays_1 = __importDefault(require("./utils/arrays"));
-var numbers_1 = __importDefault(require("./utils/numbers"));
-var strings_1 = __importDefault(require("./utils/strings"));
-var numArr = [3, 4, 5, 6];
-var wordArr = ['cat', 'dog', 'rabbit', 'bird'];
-var arrSum = arrays_1.default.addArr(numArr);
-var mixArr = arrays_1.default.concatArr(numArr, wordArr);
-var myNum = '15' % 2;
-var five = parseInt('5');
-var newArr = function (num, arr) {
-    return __spreadArray([num], arr, true);
-};
-console.log(newArr(3, wordArr));
-console.log(arrays_1.default.cut3(mixArr));
-console.log(numbers_1.default.sum(arrSum, myNum));
-console.log(strings_1.default.capitalize('the quick brown fox'));
-console.log(numbers_1.default.multiply(five, 8));
-console.log(arrays_1.default.lgNum(mixArr));
-exports.default = newArr;
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 3000;
+app.get('/api', function (req, res) {
+    res.send('Hello World');
+});
+app.listen(port, function () {
+    console.log("server is listening in port: " + port);
+});
